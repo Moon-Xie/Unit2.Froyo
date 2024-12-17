095a2b4fde919733a3cff818db67caead101dbd1
Yue Xie <moonxie9304@gmail.com>
2024-12-18 05:19:03 +0700

    added function

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ console.log(flavors);
 //The user's input string is split into an array of strings.
 const flavorsArray = flavors.split(",");
 
-//declair the object and assign value to it
-const objectFlavors = {};
-
-//A loop is used to iterate through the array of flavors.
-for(i = 0; i < flavorsArray.length; i++) {
-    let flavor = flavorsArray[i].trim();
-//An object is used to keep count of how many orders there are of each flavor.
-    if(objectFlavors[flavor]) {
-        objectFlavors[flavor]++;
-    } else {
-        objectFlavors[flavor] = 1;
+function getCountOfFlavors(){
+    //declair the object and assign value to it
+    const objectFlavors = {};
+    //A loop is used to iterate through the array of flavors.
+    for(i = 0; i < flavorsArray.length; i++) {
+        let flavor = flavorsArray[i].trim();
+    //An object is used to keep count of how many orders there are of each flavor.
+        if(objectFlavors[flavor]) {
+            objectFlavors[flavor]++;
+        } else {
+            objectFlavors[flavor] = 1;
+        }
     }
+    return objectFlavors;
 }
 
+
 //table listing how many of each flavor users have ordered.
-console.table(objectFlavors);
+console.table(getCountOfFlavors());
